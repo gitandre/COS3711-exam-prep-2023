ac60f8b31a7630b526dcf7e8b42710bde48fe2ed
gitandre <andre.jon.de.jager@gmail.com>
2024-02-08 15:08:13 +0200

    added a reference and pointer function for learning

--- 2024-Feb-RewritePrep/Chapter1/main.cpp
+++ 2024-Feb-RewritePrep/Chapter1/main.cpp
@@ -3,36 +3,87 @@
 
 using namespace std;
 
+void print(const string &val) {
+    cout << "| print()" << &val << " " << val << " |" << endl;
+}
+
+void DoReference() {
+
+    string food = "hamburger";
+    string& foodRef = food;
+    cout << food << " " << foodRef << endl;
+    cout << &food << " " << &foodRef << endl;
+
+    foodRef = "chips";
+    cout << food << " " << foodRef << endl;
+    cout << &food << " " << &foodRef << endl;
+
+//    hamburger hamburger
+//    0x5ffde0 0x5ffde0
+//
+//    chips chips
+//    0x5ffde0 0x5ffde0
+
+}
+
+void printPtr(string* paramPtr){
+    cout << *paramPtr << endl;
+    *paramPtr = "printed OK";
+}
+
+void DoPointers() {
+
+    string myString = "Andre";
+    string* stringPtr = &myString;
+    printPtr(stringPtr);
+
+    cout << *stringPtr << endl;
+
+}
+
 int main() {
 
     // We will create a logger here later using Singleton
     // todo
 
 
+//    DoReference();
+
+    DoPointers();
+
     // Print static explanation about a class
-    cout << Person::PersonExplanation() << endl;
+//    cout << Person::PersonExplanation() << endl;
+//
+//    // Instantiate Person Class Def
+//    // Very basic OOP example
+//    Person p1;
+//    Person &p1Ref = p1;
+//    p1.displayInfo();
+//    p1.haveBirthday();
+//
+//
+//    print("p1Ref prints--------------------");
+//    p1Ref.displayInfo();
+//    p1Ref.haveBirthday();
+//    print("p1Ref prints ends--------------------");
+//
+//
+//    // Instantiate Person Class Params
+//    Person p2("Joe", 18, "Student");
+//    p2.displayInfo();
+//    p2.haveBirthday();
+//
+//    // loop and create Persons
+//    print("Loop and create Persons start---------------------------\n");
+//    for (int i = 0; i <= 3; i++) {
+//        Person p;
+//        cout << "---" << endl;
+//    } // these objects go out of scope on each loop by design
+//    print("Loop and create Persons end---------------------------\n");
+//
+//    print("\nFinal Person object count");
+//    print("Person Objects = " + to_string(Person::PersonObjectCount()));
 
-    // Instantiate Person Class Def
-    // Very basic OOP example
-    Person p1;
-    p1.displayInfo();
-    p1.haveBirthday();
-
-    // Instantiate Person Class Params
-    Person p2("Joe", 18, "Student");
-    p2.displayInfo();
-    p2.haveBirthday();
-
-    // loop and create Persons
-    cout << "loop and create Persons start---------------------------\n" << endl;
-    for (int i = 0; i <= 3; i++) {
-        Person p;
-        cout << "---" << endl;
-    } // these objects go out of scope on each loop by design
-    cout << "loop and create Persons end---------------------------\n" << endl;
-
-    cout << "\nFinal Person object count" << endl;
-    cout << "Person Objects = " << Person::PersonObjectCount() << endl;
 
     return 0;
 }
