589c1f1c4ad17585d3ed5e9c706eec69931d9837
gitandre <andre.jon.de.jager@gmail.com>
2024-02-12 14:10:10 +0200

    Added xml error checking

--- 2024-Feb-RewritePrep/Chapter5/5.7Loop-n-readXML/main.cpp
+++ 2024-Feb-RewritePrep/Chapter5/5.7Loop-n-readXML/main.cpp
@@ -45,15 +45,45 @@ Title: "To Kill a Mockingbird"
 
     }
 
-    if (xml.hasError()) {
-        qDebug() << "XML Error:" << xml.errorString();
-    }
+
     /* output:
 Book ID: "1"
 Title: "Harry Potter"
 Book ID: "2"
 Title: "To Kill a Mockingbird"
  */
+
+
+    //XML error checking - broken start tag
+    qDebug() << "\nXML error checking - must parse to get errors";
+    QString xmlStringError = "library><book id='1'><title>Harry Potter</title></book><book id='2'><title>To Kill a Mockingbird</title></book></library>";
+    QXmlStreamReader xmlStreamError(xmlStringError);
+
+    // Must parse and errors get collected
+    while (!xmlStreamError.atEnd()) {
+        xmlStreamError.readNext();
+        // Check if the current element is a start element named 'book'
+        if (xmlStreamError.isStartElement() && xmlStreamError.name() == QString("book")) {
+
+            // Retrieve the 'id' attribute of the 'book' element
+            QStringView bookId = xmlStreamError.attributes().value("id");
+            qDebug() << "Book ID:" << bookId.toString();
+
+            // Read the next element, which should be the 'title' element based on the structure
+            xmlStreamError.readNext();
+            // Assuming the structure is always <book><title>Title</title></book>
+            if (xmlStreamError.isStartElement() && xmlStreamError.name() == QString("title")) {
+                qDebug() << "Title:" << xmlStreamError.readElementText();
+            }
+        }
+    }
+    if (xmlStreamError.hasError()) {
+
+        qDebug() << "XML Error:" << xmlStreamError.errorString();
+        // XML Error: "Start tag expected."
+    } else {
+        qDebug() << "No XML Error:";
+    }
 
 
     return QCoreApplication::exec();
